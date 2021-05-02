a58d02d266c1c15ab22f1e3ed58bd9ff9d148b47
pujasharma5431 <pujasharma5431@gmail.com>
2021-05-02 21:58:04 +0545

    objectsadded

---
+++ ScriptsMore/Objectdemo.js
@@ -0,0 +1,17 @@
+var dog={
+    name:"Blackforest",
+    bread:"pug",
+    color:"Black",
+    MyDog:function() {
+
+        return this.name+" "+this.color+" "+this.bread
+
+    } 
+    }
+   console.log(dog.name) 
+   console.log(dog.bread)
+   console.log(dog.color)
+
+        
+ 
+        
