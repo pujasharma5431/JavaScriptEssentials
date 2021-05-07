9e262f1f74dda2617e0528573da6399c96a3a0bc
pujasharma5431 <pujasharma5431@gmail.com>
2021-05-07 14:32:56 +0545

    Sort

---
+++ ScriptsMore/JavascriptPractice/NumberSort.js
@@ -0,0 +1,8 @@
+var num=[1,100,23,4,5,5,77,34,]
+num.sort(function(a,b)
+{
+    return(a-b)
+   
+
+})
+console.log(num)
---
+++ ScriptsMore/JavascriptPractice/StringSort.js
@@ -0,0 +1,5 @@
+var mobile=["iphone","huwai","umidigi"]
+
+console.log(mobile.sort());
+console.log(mobile.reverse())
+
