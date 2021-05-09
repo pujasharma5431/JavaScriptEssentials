8264949f3e8f3b7fbc227579e678e172429a5fcb
pujasharma5431 <pujasharma5431@gmail.com>
2021-05-09 22:05:44 +0545

    Dates

---
+++ ScriptsMore/JavascriptPractice/Date/Year.js
@@ -0,0 +1,11 @@
+  var date=new Date()
+// console.log(date.toString())
+// console.log(date.toUTCString())
+// console.log(date.getMonth()+1)
+var day=date.getDay();
+var days=["Sunday","Monday","Tuesday","Wednesday"]
+console.log(days[day]);
+
+
+
+
