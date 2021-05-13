8334302b7b94c49d5c6f36d76a1cad3ebeed0895
pujasharma5431 <pujasharma5431@gmail.com>
2021-05-13 19:27:15 +0545

    Loops

---
+++ ScriptsMore/JavascriptPractice/Loop/ForInArray.js
@@ -0,0 +1,18 @@
+
+var fruit={
+  
+    name:"Kiwi",
+    Cost:"20000",
+    color:"Green"
+}
+
+var x;
+var text=" "
+
+for (x in fruit){
+
+
+text=text+fruit[x]+" ";
+}
+console.log(text)
+
---
+++ ScriptsMore/JavascriptPractice/Loop/ForLoop.js
@@ -0,0 +1,6 @@
+var num;
+for(let num=0;num<=10;num++)
+{
+    console.log(num)
+    
+}
