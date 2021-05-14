7c87ccd73ac9102b11a9a8cd90544a1c37eadfeb
pujasharma5431 <pujasharma5431@gmail.com>
2021-05-14 23:25:31 +0545

    Loop

---
+++ ScriptsMore/JavascriptPractice/Loop/Break.js
@@ -0,0 +1,9 @@
+for(let i=0;i<6;i++)
+{
+    if(i==3){
+        break;
+
+    }
+    
+    console.log(i)
+}
---
+++ ScriptsMore/JavascriptPractice/Loop/Continu.js
@@ -0,0 +1,9 @@
+for(let i=0;i<6;i++)
+{
+    if(i==3){
+       continue;
+
+    }
+    
+    console.log(i)
+}
