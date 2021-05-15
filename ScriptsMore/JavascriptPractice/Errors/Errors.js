0fe22473271927756bdd2bd9b0ea1e5b990dc8e6
pujasharma5431 <pujasharma5431@gmail.com>
2021-05-15 17:54:42 +0545

    Errors

---
+++ ScriptsMore/JavascriptPractice/Errors/Errors.js
@@ -0,0 +1,19 @@
+var a="Pooja";
+if (a!=undefined)
+{
+    try{
+        console.log("Hello")
+        functionPoo();
+
+
+    }
+catch {
+    console.log("Please Check it Again")
+
+}
+finally{
+    console.log("You did a great job")
+    
+}
+
+}
